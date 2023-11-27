ad842d0ca0a8b0f3f9ef5bb3c834d81891aba227
Ismael Chaves Mizael <125465461+Ismael-Oficial@users.noreply.github.com>
2023-11-27 19:44:22 -0300

    Atualização v1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Services from './componentes/Services';
 import Equip from './componentes/Equip';
 import Clientes from './componentes/Clientes';
 import Portifolio from './componentes/Portifolio';
-import Depoiment from './componentes/Depoiment';
+
 import WorkUs from './componentes/WorkUs';
 import Contact from './componentes/Contact';
 
@@ -23,7 +23,7 @@ function App() {
       <Equip />
       <Clientes />
       <Portifolio />
-      <Depoiment />
+
       <div className="for-sale">
         <p>Desconto para pequenas empresas de</p>
         <h2>ATÉ 80% OFF</h2>
